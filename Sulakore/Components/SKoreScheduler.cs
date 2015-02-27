9be122ba2ada6a3caf04dfbf2d4e1327f02d70ef
ArachisH <TheOldNutman@gmail.com>
2015-02-26 20:52:36 -0800

    Re-did all the things, sorta. Now inherits from SKoreListView.

--- Sulakore/Components/SKoreScheduler.cs
+++ Sulakore/Components/SKoreScheduler.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Drawing;
-using System.Collections;
 using System.Windows.Forms;
 using System.ComponentModel;
 using System.Collections.Generic;
@@ -9,217 +7,180 @@ using Sulakore.Protocol;
 
 namespace Sulakore.Components
 {
-    public class SKoreScheduler : ListView
+    public class SKoreScheduler : SKoreListView
     {
-        #region Subscribable Events
         public event EventHandler<HScheduleTriggeredEventArgs> ScheduleTriggered;
-        private void OnScheduleTriggered(object sender, HScheduleTriggeredEventArgs e)
-        {
-            if (ScheduleTriggered != null)
-            {
-                ScheduleTriggered(sender, e);
-                if (e.Cancel)
-                {
-                    SchedulesRunning--;
-                    _bySchedule[(HSchedule)sender].SubItems[4].Text = StatusSTOPPED;
-                }
-            }
-        }
-        #endregion
 
-        #region Private Fields
-        private bool _suppressSelectionChanged;
-
-        private readonly Dictionary<HSchedule, ListViewItem> _bySchedule;
+        private readonly Dictionary<HSchedule, ListViewItem> _items;
         private readonly Dictionary<ListViewItem, HSchedule> _schedules;
+        private readonly Dictionary<ListViewItem, string> _descriptions;
 
-        private const string StatusRUNNING = "Running", StatusSTOPPED = "Stopped";
-        #endregion
+        private const string RUNNING = "Running", STOPPED = "Stopped";
 
-        #region Public Properties
-        public bool LockColumns { get; set; }
-        public int SchedulesRunning { get; private set; }
-        #endregion
+        [DefaultValue(true)]
+        public bool AutoStart { get; set; }
+        public int Running { get; private set; }
 
-        #region Constructor(s)
         public SKoreScheduler()
         {
+            _items = new Dictionary<HSchedule, ListViewItem>();
+            _descriptions = new Dictionary<ListViewItem, string>();
             _schedules = new Dictionary<ListViewItem, HSchedule>();
-            _bySchedule = new Dictionary<HSchedule, ListViewItem>();
 
-            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
-            SetStyle(ControlStyles.EnableNotifyMessage, true);
-
-            var packetCol = new ColumnHeader { Name = "PacketCol", Text = "Packet", Width = 131 };
-            var directionCol = new ColumnHeader { Name = "DirectionCol", Text = "Direction", Width = 63 };
-            var burstCol = new ColumnHeader { Name = "BurstCol", Text = "Burst", Width = 44 };
-            var intervalCol = new ColumnHeader { Name = "IntervalCol", Text = "Interval", Width = 58 };
-            var statusCol = new ColumnHeader { Name = "StatusCol", Text = "Status", Width = 62 };
-            Columns.AddRange(new[] { packetCol, directionCol, burstCol, intervalCol, statusCol });
-
-            FullRowSelect = true;
-            GridLines = true;
-            HeaderStyle = ColumnHeaderStyle.Nonclickable;
-            MultiSelect = false;
-            ShowItemToolTips = true;
-            Size = new Size(386, 141);
-            UseCompatibleStateImageBehavior = false;
-            View = View.Details;
-            LockColumns = true;
-        }
-        #endregion
-
-        #region Public Methods
-        public void StopAll()
-        {
-            BeginUpdate();
-            foreach (ListViewItem item in _schedules.Keys)
-            {
-                _schedules[item].Stop();
-                item.SubItems[4].Text = StatusSTOPPED;
-            }
-            EndUpdate();
-
-            SchedulesRunning = 0;
-        }
-        public void StartAll()
-        {
-            BeginUpdate();
-            foreach (ListViewItem item in _schedules.Keys)
-            {
-                _schedules[item].Start();
-                item.SubItems[4].Text = StatusRUNNING;
-            }
-            EndUpdate();
-
-            SchedulesRunning = _schedules.Count;
-        }
-        public void ToggleSelected()
-        {
-            ListViewItem selectedItem = SelectedItems[0];
-            if (_schedules.ContainsKey(selectedItem))
-            {
-                HSchedule schedule = _schedules[selectedItem];
-                bool shouldStop = (schedule.IsRunning);
-
-                if (shouldStop)
-                {
-                    schedule.Stop();
-                    SchedulesRunning--;
-                }
-                else
-                {
-                    schedule.Start();
-                    SchedulesRunning++;
-                }
-                selectedItem.SubItems[4].Text = (shouldStop ? StatusSTOPPED : StatusRUNNING);
-            }
-        }
-        public void RemoveSelected()
-        {
-            ListViewItem selectedItem = SelectedItems[0];
-            if (_schedules.ContainsKey(selectedItem))
-            {
-                HSchedule schedule = _schedules[selectedItem];
-
-                if (schedule.IsRunning) SchedulesRunning--;
-                schedule.Dispose();
-
-                _schedules.Remove(selectedItem);
-                _bySchedule.Remove(schedule);
-
-                int index = SelectedIndices[0];
-                _suppressSelectionChanged = (Items.Count > 1);
-                Items.RemoveAt(index);
-
-                if (Items.Count > 0)
-                {
-                    _suppressSelectionChanged = true;
-                    Items[index - (index < Items.Count - 1 && index != 0 || index == Items.Count ? 1 : 0)].Selected = true;
-                }
-            }
-        }
-        public HSchedule GetSelected()
-        {
-            return _schedules[SelectedItems[0]];
+            AutoStart = true;
+            CheckBoxes = true;
         }
 
-        public string GetSelectedDescription()
+        protected override void RemoveItem(ListViewItem listViewItem)
         {
-            string desc = _bySchedule[GetSelected()].ToolTipText;
-            return !string.IsNullOrEmpty(desc) ? desc.Substring(13).Split('\n')[0] : string.Empty;
+            _schedules[listViewItem].Dispose();
+            _schedules.Remove(listViewItem);
+            _descriptions.Remove(listViewItem);
+
+            base.RemoveItem(listViewItem);
         }
-        public void SetSelectedDescription(string description)
+        public void AddSchedule(HMessage packet, int burst, int interval, string description)
         {
-            if (!string.IsNullOrEmpty(description))
-                _bySchedule[GetSelected()].ToolTipText = string.Format("Description: {0}\n{1}",
-                    description, GetSelected().Packet);
-        }
+            if (packet.IsCorrupted || packet.Destination == HDestination.Unknown)
+                throw new Exception("Provided packet is either corrupted, or has Destination set to 'Unknown'.");
 
-        public void AddSchedule(HSchedule schedule, bool autoStart, string description)
-        {
-            if (_schedules.ContainsValue(schedule)) return;
+            var item = new ListViewItem(new[] { packet.ToString(),
+                packet.Destination.ToString(), burst.ToString(), interval.ToString(), AutoStart ? RUNNING : STOPPED });
 
-            var item = new ListViewItem(new string[]
-            {
-                schedule.Packet.ToString(),
-                schedule.Packet.Destination.ToString(),
-                schedule.Burst.ToString(),
-                schedule.Interval.ToString(),
-                autoStart ? StatusRUNNING : StatusSTOPPED
-            });
-
-            if (!string.IsNullOrEmpty(description))
-                item.ToolTipText = string.Format("Description: {0}\n{1}",
-                    description, schedule.Packet);
-
-            Focus();
-            Items.Add(item);
-            _suppressSelectionChanged = Items.Count > 1;
-            item.Selected = true;
-            EnsureVisible(Items.Count - 1);
-
-            _schedules.Add(item, schedule);
-            _bySchedule.Add(schedule, item);
-
+            var schedule = new HSchedule(packet, interval, burst);
             schedule.ScheduleTriggered += OnScheduleTriggered;
-            if (autoStart)
-            {
-                SchedulesRunning++;
-                schedule.Start();
-            }
-        }
-        #endregion
 
-        #region Overrided Methods
-        protected override void OnNotifyMessage(Message m)
-        {
-            if (m.Msg != 0x14)
-                base.OnNotifyMessage(m);
+            _items.Add(schedule, item);
+            _schedules.Add(item, schedule);
+            _descriptions.Add(item, description);
+
+            item.Checked = AutoStart;
+            item.ToolTipText = description;
+
+            FocusAdd(item);
         }
-        protected override void OnMouseDown(MouseEventArgs e)
+
+        public void StopAllSchedules()
         {
-            _suppressSelectionChanged = (GetItemAt(e.X, e.Y) != null);
-            base.OnMouseDown(e);
+            foreach (ListViewItem item in _schedules.Keys)
+                item.Checked = false;
         }
-        protected override void OnColumnWidthChanging(ColumnWidthChangingEventArgs e)
+        public void StartAllSchedules()
         {
-            if (LockColumns)
+            foreach (ListViewItem item in _schedules.Keys)
+                item.Checked = true;
+        }
+
+        public int GetItemBurst()
+        {
+            return SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Burst : 0;
+        }
+        public void SetItemBurst(int burst)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Burst = burst;
+            item.SubItems[2].Text = burst.ToString();
+        }
+
+        public int GetItemInterval()
+        {
+            return SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Interval : 0;
+        }
+        public void SetItemInterval(int interval)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Interval = interval;
+            item.SubItems[2].Text = interval.ToString();
+        }
+
+        public string GetItemDescription()
+        {
+            return SelectedItems.Count > 0 ?
+                _descriptions[SelectedItems[0]] : string.Empty;
+        }
+        public void SetItemDescription(string description)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            item.ToolTipText = description;
+            _descriptions[item] = description;
+        }
+
+        public HMessage GetItemPacket()
+        {
+            return SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Packet : null;
+        }
+        public void SetItemPacket(HMessage packet)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Packet = packet;
+            item.SubItems[0].Text = packet.ToString();
+            item.SubItems[1].Text = packet.Destination.ToString();
+        }
+
+        public HDestination GetItemDestination()
+        {
+            return SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Packet.Destination : HDestination.Unknown;
+        }
+        public void SetItemDestination(HDestination destination)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Packet.Destination = destination;
+            item.SubItems[1].Text = destination.ToString();
+        }
+
+        protected override void OnItemChecked(ItemCheckedEventArgs e)
+        {
+            if (!_schedules.ContainsKey(e.Item)) return;
+
+            HSchedule schedule = _schedules[e.Item];
+            e.Item.SubItems[4].Text = e.Item.Checked ? RUNNING : STOPPED;
+
+            if (e.Item.Checked) schedule.Start();
+            else schedule.Stop();
+
+            base.OnItemChecked(e);
+        }
+        protected virtual void OnScheduleTriggered(object sender, HScheduleTriggeredEventArgs e)
+        {
+            EventHandler<HScheduleTriggeredEventArgs> handler = ScheduleTriggered;
+            if (handler != null)
             {
-                e.Cancel = true;
-                e.NewWidth = Columns[e.ColumnIndex].Width;
+                try { handler(sender, e); }
+                catch { e.Cancel = true; }
+                finally
+                {
+                    if (e.Cancel)
+                    {
+                        ListViewItem item = _items[(HSchedule)sender];
+                        item.SubItems[4].Text = STOPPED;
+                        item.Checked = false;
+                    }
+                }
             }
-            base.OnColumnWidthChanging(e);
         }
-        protected override void OnItemSelectionChanged(ListViewItemSelectionChangedEventArgs e)
+
+        protected override void Dispose(bool disposing)
         {
-            if (_suppressSelectionChanged && !e.IsSelected) _suppressSelectionChanged = false;
-            else
+            if (disposing)
             {
-                base.OnItemSelectionChanged(e);
-                if (e.IsSelected) _suppressSelectionChanged = false;
+                foreach (HSchedule schedule in _schedules.Values)
+                    schedule.Dispose();
             }
+            base.Dispose(disposing);
         }
-        #endregion
     }
 }
