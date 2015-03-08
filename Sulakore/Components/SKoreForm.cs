0d5f206634c2111eb042cb435abbd31a6fdb3cd5
ArachisH <TheOldNutman@gmail.com>
2015-03-08 16:14:12 -0700

    Removed some property changes in Constructor.

---
+++ Sulakore/Components/SKoreForm.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+using Sulakore.Extensions;
+
+namespace Sulakore.Components
+{
+    public class SKoreForm : Form
+    {
+        /// <summary>
+        /// Gets or sets the extension for this instance.
+        /// </summary>
+        public ExtensionBase Extension { get; set; }
+
+        public SKoreForm()
+        { }
+        public SKoreForm(ExtensionBase extension)
+            : this()
+        {
+            Extension = extension;
+        }
+    }
+}
