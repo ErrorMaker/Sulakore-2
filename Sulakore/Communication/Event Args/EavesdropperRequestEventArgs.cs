08ebb36f87ceec492488c5ae7e9f0153280ceefd
ArachisH <TheOldNutman@gmail.com>
2015-03-08 17:39:35 -0700

    Added new Eavesdropper event arguments.

---
+++ Sulakore/Communication/Event Args/EavesdropperRequestEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.ComponentModel;
+
+namespace Sulakore.Communication
+{
+    public class EavesdropperRequestEventArgs : CancelEventArgs
+    {
+        private readonly WebRequest _request;
+        public WebRequest Request
+        {
+            get { return _request; }
+        }
+
+        private byte[] _payload;
+        /// <summary>
+        /// Gets or sets the request data being sent.
+        /// </summary>
+        public byte[] Payload
+        {
+            get { return _payload; }
+            set
+            {
+                _payload = value;
+                _request.ContentLength = _payload.Length;
+            }
+        }
+
+        public EavesdropperRequestEventArgs(HttpWebRequest request)
+        {
+            _request = request;
+        }
+    }
+}
---
+++ Sulakore/Communication/Event Args/EavesdropperResponseEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.ComponentModel;
+
+namespace Sulakore.Communication
+{
+    public class EavesdropperResponseEventArgs : CancelEventArgs
+    {
+        private readonly WebResponse _response;
+        public WebResponse Response
+        {
+            get { return _response; }
+        }
+
+        private byte[] _payload;
+        /// <summary>
+        /// Gets or sets the response data being received
+        /// </summary>
+        public byte[] Payload
+        {
+            get { return _payload; }
+            set
+            {
+                _payload = value;
+                _response.Headers["Content-Length"] = _payload.Length.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value that determines whether Eavesdropper will terminate once this response has been processed.
+        /// </summary>
+        public bool ShouldTerminate { get; set; }
+
+        public EavesdropperResponseEventArgs(WebResponse response)
+        {
+            _response = response;
+        }
+    }
+}
